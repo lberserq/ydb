586b68862ebfb2921ecf4255f46a6aff922e2e40
Kamil Khamitov <kkhamitov@yandex-team.ru>
2026-02-02 20:04:09 +0300

    (-) refix

--- ydb/core/actors_trace_info/actors_trace_info.cpp
+++ ydb/core/actors_trace_info/actors_trace_info.cpp
@@ -13,7 +13,7 @@ namespace NKikimr::NActorsTraceInfo {
 class TActorsTraceProviderService : public TActorBootstrapped<TActorsTraceProviderService> {
 public:
     static constexpr NKikimrServices::TActivity::EType ActorActivityType() {
-        return NKikimrServices::TActivity::MONITORING_SERVICE; 
+        return NKikimrServices::TActivity::MONITORING_SERVICE;
     }
 
     TActorsTraceProviderService()= default;
@@ -22,14 +22,33 @@ public:
         Become(&TActorsTraceProviderService::StateWork);
     }
 
-    void Handle(TEvActorsTraceInfoRequest::TPtr& ev, const TActorContext& ctx) {
-        Y_UNUSED(ev);
-        Y_UNUSED(ctx);
+    void Handle(TEvActorsTraceStartRequest::TPtr& ev, const TActorContext& ctx) {
+        THolder<TEvActorsTraceStartResponse> response = MakeHolder<TEvActorsTraceStartResponse>();
+        ctx.ActorSystem()->GetActorTracer()->Start();
+        ctx.Send(ev->Sender, response.Release(), 0, ev->Cookie);
+    }
+
+    void Handle(TEvActorsTraceGetInfoRequest::TPtr& ev, const TActorContext& ctx) {
+        THolder<TEvActorsTraceGetInfoResponse> response = MakeHolder<TEvActorsTraceGetInfoResponse>();
+        auto& record = response->Record;
+        ctx.ActorSystem()->GetActorTracer()->Stop();
+        auto data = ctx.ActorSystem()->GetActorTracer()->GetTraceData();
+        {
+            auto header = NActors::NTracing::SerializeHeader(std::move(data.ActivityDict), std::move(data.EventNamesDict));
+            *record.MutableHeader() = std::move(TString{header.Begin(), header.End()});
+        }
+
+        {
+            auto events = NActors::NTracing::SerializeEvents(std::move(data.Events));
+            *record.MutableEvents() = std::move(TString{events.Begin(), events.End()});
+        }
+        ctx.Send(ev->Sender, response.Release(), 0, ev->Cookie);
     }
 
     void StateWork(TAutoPtr<NActors::IEventHandle>& ev) {
         switch (ev->GetTypeRewrite()) {
-            HFunc(TEvActorsTraceInfoRequest, Handle);
+            HFunc(TEvActorsTraceStartRequest, Handle);
+            HFunc(TEvActorsTraceGetInfoRequest, Handle);
             cFunc(TEvents::TSystem::PoisonPill, PassAway);
         }
     }
--- ydb/core/actors_trace_info/actors_trace_info.h
+++ ydb/core/actors_trace_info/actors_trace_info.h
@@ -5,15 +5,22 @@
 
 namespace NKikimr::NActorsTraceInfo {
 enum EEv {
-    EvActorsTraceRequest = EventSpaceBegin(TKikimrEvents::ES_ACTORS_TRACE_INFO),
-    EvActorsTraceResponse,
+    EvActorsTraceStartRequest = EventSpaceBegin(TKikimrEvents::ES_ACTORS_TRACE_INFO),
+    EvActorsTraceStartResponse,
+    EvActorsTraceGetInfoRequest,
+    EvActorsTraceGetInfoResponse,
     EvEnd
 };
 
 static_assert(EvEnd < EventSpaceEnd(TKikimrEvents::ES_ACTORS_TRACE_INFO), "expect EvEnd < EventSpaceEnd(TKikimrEvents::ES_ACTORS_TRACE_INFO)");
 
-struct TEvActorsTraceInfoRequest : public TEventPB<TEvActorsTraceInfoRequest, NKikimrActorsTraceInfoProto::TEvActorsTraceInfoRequest, EvActorsTraceRequest> {};
-struct TEvActorsTraceInfoResponse : public TEventPB<TEvActorsTraceInfoResponse, NKikimrActorsTraceInfoProto::TEvActorsTraceInfoResponse, EvActorsTraceResponse> {};
+struct TEvActorsTraceStartRequest: public TEventPB<TEvActorsTraceStartRequest, NKikimrActorsTraceInfoProto::TEvActorsTraceStartRequest, EvActorsTraceStartRequest> {};
+
+struct TEvActorsTraceStartResponse: public TEventPB<TEvActorsTraceStartResponse, NKikimrActorsTraceInfoProto::TEvActorsTraceStartResponse, EvActorsTraceStartResponse> {};
+
+struct TEvActorsTraceGetInfoRequest: public TEventPB<TEvActorsTraceGetInfoRequest, NKikimrActorsTraceInfoProto::TEvActorsTraceGetInfoRequest, EvActorsTraceGetInfoRequest> {};
+
+struct TEvActorsTraceGetInfoResponse: public TEventPB<TEvActorsTraceGetInfoResponse, NKikimrActorsTraceInfoProto::TEvActorsTraceGetInfoResponse, EvActorsTraceGetInfoResponse> {};
 
 inline NActors::TActorId MakeActorsTraceProviderServiceID(ui32 nodeId) { return NActors::TActorId(nodeId, "actorTraceInfo"); }
 IActor* CreateActorsTraceProviderService();
